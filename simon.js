27423a8e737abbf697d9f81f1b6d55e469c90c59
Lipathor <pawel@lipka.net.pl>
2013-02-25 13:17:16 +0000

    More async.

--- simon.js
+++ simon.js
@@ -7,11 +7,13 @@
  * http://lipka.net.pl || http://trewebs.com
  */
 
-function simon(db, server){
-    this.mongo = require("mongodb");
-    ObjectID = require("mongodb").ObjectID;
-    Q = require('q');
+var ee = new (require('events').EventEmitter);
 
+
+
+function simon(db, server,fn){
+    this.mongo = require("mongodb");
+	simon.fn = fn || function(){};
     if(server == undefined){
         server = {};
         server.addr = "127.0.0.1";
@@ -22,111 +24,103 @@ function simon(db, server){
     server.addr = server.addr || "127.0.0.1";
     server.port = server.port || 27017;
     db = db || "local";
-    this.client = new this.mongo.Db(db,new this.mongo.Server(server.addr,server.ip,{}),{w:1});
-
-    simon.prototype.get = function(collection,id,fields,sort,pagination,callback) {
-        id = id || {};
-        fields = fields || {};
-        sort = sort || {};
-        pagination = pagination || {};
-        callback = callback || function(){};
-
-            this.client.open(function(err,p_client){
-                if(err) throw err;
-                p_client.collection(collection,function(err, collection){
-                    if(err) throw err;
-                    var find;
-
-                    if(typeof(id) == "object") {
-		    	        find = id;
-                    }
-                    else if(id == "-"){
-                        find = {}
-                    }
-                    else {
-                        find = {_id:new ObjectID(id)};
-                    }
-
-                    if(typeof(fields) == "function") {
-                        callback = fields;
-                        fields = {};
-                        sort = {};
-                        pagination = {};
-                    }
-
-                    if(typeof(sort) == "function") {
-                        callback = sort;
-                        sort = {};
-                        pagination = {};
-                    }
-
-                    if(typeof(pagination) == "function") {
-                        callback = pagination;
-                        pagination = {};
-                    }
-
-                    collection.find(find,{limit: pagination.limit, skip: pagination.offset, sort: sort, fields: fields}).toArray(function(err,docs){
-                        p_client.close(function(a,b){
-                            callback(err,docs);    
-                        });
-                    });
-                });
-            });
-    }
+    this.client = new this.mongo.Db(db,new this.mongo.Server(server.addr,server.port,{}),{w:1});
     
-    simon.prototype.post = function(collection,docs,callback) {
-        this.client.open(function(err,p_client){
-            if(err) throw err;
-            p_client.collection(collection,function(err,collection){
-                if(err) throw err;
-                
-                collection.insert(docs,function(err,docs){
-                    p_client.close(function(a,b){
-                        callback(err,docs);    
-                    });
-                });    
-            })
-        });
-    }
-    
-    simon.prototype.delete = function(collection,id,callback){
-        id = id || '-';
-        this.client.open(function(err,p_client){
-            if(err) throw err;
-            
-            p_client.collection(collection,function(err,collection){
-                if(err) throw err;
-                var find;
-                    if(id == '-')
-                        find = {};
-		    else if(typeof(id) == "object")
-			find = id;
-                    else
-                        find = {_id:new ObjectID(id)};
-                collection.remove(find,function(err,removed){
-                    p_client.close(function(a,b){
-                        callback(err,removed);    
-                    });
-                });
-            });
-        });
-    }
-    
-    simon.prototype.put = function(collection,id,doc,callback) {
-        this.client.open(function(err,p_client){
-            if(err) throw err;
-            p_client.collection(collection,function(err,collection){
-                if(err) throw err;
 
-                var find = {_id:new ObjectID(id.toString())};
-                collection.update(find,doc,function(err,docs){
-                    p_client.close(function(a,b){
-                        callback(err,docs);    
-                    });
-                });    
-            })
-        });
-    }
+    this.client.open(function(err,p_client){
+    if(err) throw err;
+    ee.emit("opened",p_client);
+ });
 }
+
+ee.on("opened",function(p_client){
+var ObjectID = require("mongodb").ObjectID;
+var Q = require('q');
+
+var cruds = {};
+cruds.get = function(collection,id,fields,sort,pagination,callback) {
+        id = id || {};
+	fields = fields || {};
+	sort = sort || {};
+	pagination = pagination || {};
+	callback = callback || function(){};
+
+	p_client.collection(collection,function(err, collection){
+		if(err) throw err;
+		var find;
+		
+		if(typeof(id) == "object") {
+			find = id;
+		}
+		
+		else if(id == "-"){
+			find = {}
+		}
+																													                    else {
+			find = {_id:new ObjectID(id)};
+		}
+	
+		if(typeof(fields) == "function") {
+			callback = fields;
+			fields = {};
+			sort = {};
+			pagination = {};
+		}
+
+		if(typeof(sort) == "function") {
+			callback = sort;
+			sort = {};
+			pagination = {};
+		}
+	
+		if(typeof(pagination) == "function") {
+			callback = pagination;
+			pagination = {};
+		}
+
+		collection.find(find,{limit: pagination.limit, skip: pagination.offset, sort: sort, fields: fields}).toArray(function(err,docs){
+			callback(err,docs);
+		});
+	});
+}
+
+cruds.post = function(collection,docs,callback) {
+     p_client.collection(collection,function(err,collection){
+	if(err) throw err;
+	collection.insert(docs,function(err,docs){
+		callback(err,docs);
+	});
+    })
+}
+
+cruds.delete = function(collection,id,callback){
+        id = id || '-';
+        p_client.collection(collection,function(err,collection){
+		if(err) throw err;
+	        var find;
+		if(id == '-')
+		find = {};
+		else if(typeof(id) == "object")
+		find = id;
+		else
+		find = {_id:new ObjectID(id)};
+		collection.remove(find,function(err,removed){
+			callback(err,removed);
+		});
+	});
+}
+
+cruds.put = function(collection,id,doc,callback) {
+	p_client.collection(collection,function(err,collection){
+		if(err) throw err;
+		var find = {_id:ObjectID(id.toString())};
+		collection.update(find,doc,function(err,docs){
+			callback(err,docs);
+		});
+	})
+}
+
+simon.fn(cruds);
+});
 
 exports.simon = simon;
--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
 var assert = require('chai').assert,
     expect = require('chai').expect;
 var simon = require('../simon.js');
-test = new simon.simon();
 var ids = [];
 
 var testdata = [{name: "PL", age: 27},
@@ -15,56 +14,69 @@ var testdata = [{name: "PL", age: 27},
 describe('Post',function(){
     
     it('should insert seven new docs into collection',function(done){
-        test.post("test",testdata,function(err,docs){
+        simon.simon("local",{addr:"127.0.0.1",port:27017},function(test){
+	test.post("test",testdata,function(err,docs){
             ids = [docs[0]["_id"],docs[1]["_id"], docs[2]["_id"], docs[3]["_id"], docs[4]["_id"], docs[5]["_id"], docs[6]["_id"]];
             assert.lengthOf(docs,7,'seven docs inserted');
             done();
         });    
     })
-    
+  });  
 });
 
 describe('Get',function(){
     
     it('should return seven elements',function(done){
-        test.get("test","-",function(err,docs){
+     simon.simon("local",{addr:"127.0.0.1",port:27017},function(test){
+     	test.get("test","-",function(err,docs){
             assert.lengthOf(docs,7,'seven docs got');
             done();
         });    
+	});
     });
 
     it('should return one element with query request',function(done){
-    	test.get("test",{name:"LL"},function(err,docs){
+    	simon.simon("local",{addr:"127.0.0.1",port:27017},function(test){
+	test.get("test",{name:"LL"},function(err,docs){
 		    assert.lengthOf(docs,1,'one doc got');
 		    done();
+	    });
 	    });
     });
 
     it('should return only age fields', function(done){
-        test.get("test","-",{age:1}, function(err,docs){
+        simon.simon("local",{addr:"127.0.0.1",port:27017},function(test){
+
+	test.get("test","-",{age:1}, function(err,docs){
             for(var doc in docs) {
                 assert.isDefined(docs[doc].age,'Age field found');
                 assert.isUndefined(docs[doc].name, 'Name field not found (and it is OK)');
             }
          done();
         });
+	});
     });
 
     it('should be sorted by age', function(done){
-        test.get("test","-",{},{age:1},function(err,docs){
+        simon.simon("local",{addr:"127.0.0.1",port:27017},function(test){
+
+	test.get("test","-",{},{age:1},function(err,docs){
             for(var i=0; i<7; i++) {
                 if(i<6)
                 assert(docs[i+1].age >= docs[i].age, 'Elements in good order');
             }
             done();
         });
+	});
     });
 
 });
 
 describe('Put',function(){
+
     it('should change age of people',function(done){
-        test.put("test",ids[0],{name:"Lipathor",age:28},function(err,doc){
+      simon.simon("local",{addr:"127.0.0.1",port:27017},function(test){
+      test.put("test",ids[0],{name:"Lipathor",age:28},function(err,doc){
             test.put("test",ids[1],{name:"LL",age:20},function(err,doc){
                 test.get("test","-",function(err,docs){
                    assert.equal(docs[0].age,20,'Age is 20');
@@ -73,15 +85,19 @@ describe('Put',function(){
                 });
             });
         });    
-    });    
+    }); 
+    });
 });
 
 describe('Delete',function(){
 
     it('should return seven docs are removed',function(done){
+    simon.simon("local",{addr:"127.0.0.1",port:27017},function(test){
+
         test.delete("test","-",function(err,removed){
             assert.equal(removed,7,'seven docs removed');
             done();
         });    
-    });    
+    });
+    });
 });
